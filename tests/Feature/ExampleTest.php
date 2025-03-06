d9c75f3607e676949d3d452592fb9a7c76aeb51d
FlameXode <44475239+WovenCoast@users.noreply.github.com>
2025-03-06 10:00:06 +0500

    Initial commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Javaabu\{YourPackage}\Tests\Feature;
+
+use Javaabu\{YourPackage}\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class ExampleTest extends TestCase
+{
+    #[Test]
+    public function it_can_test_the_feature_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Javaabu\{YourPackage}\Tests\Unit;
+
+use Javaabu\{YourPackage}\Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+class ExampleTest extends TestCase
+{
+    #[Test]
+    public function it_can_test_it_is_true(): void
+    {
+        $this->assertTrue(true);
+    }
+}
