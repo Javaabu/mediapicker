58175ad51e5394e461a09b57cd04ff865a12cd85
Arushad Ahmed <dash-8x@hotmail.com>
2025-04-14 14:46:51 +0500

    - Added attachments clean command

--- tests/Feature/Commands/AttachmentsCleanCommandTest.php
+++ tests/Feature/Commands/AttachmentsCleanCommandTest.php
@@ -3,6 +3,8 @@
 namespace Javaabu\Mediapicker\Tests\Feature\Commands;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 use Javaabu\Mediapicker\Tests\TestCase;
 use PHPUnit\Framework\Attributes\Test;
 
@@ -11,28 +13,20 @@ class AttachmentsCleanCommandTest extends TestCase
     use RefreshDatabase;
 
     #[Test]
-    public function it_can_clean_all_attachments(): void
-    {
-        $attachment = $this->getAttachment();
-
-        $this->assertDatabaseHas('attachments', [
-            'id' => $attachment->id,
-        ]);
-
-        $this->artisan('mediapicker:clear')
-            ->assertSuccessful();
-
-        $this->assertDatabaseEmpty('attachments');
-    }
-
-    /*#[Test]
-    public function it_can_clear_attachments_of_a_specific_model_type(): void
+    public function it_can_clean_orphaned_attachments(): void
     {
         $post = $this->getModel();
         $attachment = $this->getAttachment($post);
 
-        $other_model = $this->getModelWithConversions();
-        $attachment_2 = $this->getAttachment($other_model);
+        DB::table('posts')
+            ->where('id', $post->id)
+            ->delete();
+
+        $attachment_2 = $this->getAttachment();
+
+        $this->assertDatabaseMissing('posts', [
+            'id' => $post->id,
+        ]);
 
         $this->assertDatabaseHas('attachments', [
             'id' => $attachment->id,
@@ -42,7 +36,7 @@ class AttachmentsCleanCommandTest extends TestCase
             'id' => $attachment_2->id,
         ]);
 
-        $this->artisan('mediapicker:clear', ['modelType' => 'post'])
+        $this->artisan('mediapicker:clean', ['--delete-orphaned' => true])
             ->assertSuccessful();
 
         $this->assertDatabaseMissing('attachments', [
@@ -55,31 +49,36 @@ class AttachmentsCleanCommandTest extends TestCase
     }
 
     #[Test]
-    public function it_can_clear_attachments_of_a_specific_collection(): void
+    public function it_can_clean_deprecated_attachment_conversion_files(): void
     {
-        $post = $this->getModel();
-        $attachment = $this->getAttachment($post, 'test1');
+        $model = $this->getModelWithConversions();
+        $media = $this->getMedia();
 
-        $other_model = $this->getModelWithConversions();
-        $attachment_2 = $this->getAttachment($other_model, 'test');
+        $attachment = $model->addAttachment($media)
+                            ->toAttachmentCollection();
+
+        $path = $model->getFirstAttachmentPath(conversionName: 'test');
+
+        $this->assertEquals($this->getMediaDirectory($media->getKey() . '/conversions/test-test.jpg'), $path);
+        $this->assertFileExists($path);
+
+        $new_path = $this->getMediaDirectory($media->getKey() . '/conversions/test-test2.jpg');
+        File::copy($path, $new_path);
+
+        $this->assertFileExists($new_path);
 
         $this->assertDatabaseHas('attachments', [
             'id' => $attachment->id,
         ]);
 
-        $this->assertDatabaseHas('attachments', [
-            'id' => $attachment_2->id,
-        ]);
-
-        $this->artisan('mediapicker:clear', ['collectionName' => 'test1'])
+        $this->artisan('mediapicker:clean')
             ->assertSuccessful();
 
-        $this->assertDatabaseMissing('attachments', [
+        $this->assertDatabaseHas('attachments', [
             'id' => $attachment->id,
         ]);
 
-        $this->assertDatabaseHas('attachments', [
-            'id' => $attachment_2->id,
-        ]);
-    }*/
+        $this->assertFileExists($path);
+        $this->assertFileDoesNotExist($new_path);
+    }
 }
